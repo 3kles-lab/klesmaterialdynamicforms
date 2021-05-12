7bd929a419137180494350acd979e794c402fba4
Charles Da Silva Costa <cdasilvacosta@3kles-consulting.com>
2021-05-12 18:43:00 +0200

    Fix group field css

--- projects/kles-material-dynamicforms/src/lib/fields/group.component.ts
+++ projects/kles-material-dynamicforms/src/lib/fields/group.component.ts
@@ -4,15 +4,16 @@ import { KlesFieldAbstract } from './field.abstract';
 @Component({
     selector: 'kles-group',
     template: `
-    <div [formGroup]="group">
-        <div [formGroupName]="field.name">
-            <ng-container *ngFor="let subfield of field.collections;" class="dynamic-form-row-item" klesDynamicField [field]="subfield" [group]="subGroup">
+    <div [formGroup]="group" class="group-container">
+        <div [formGroupName]="field.name" class="group-container" [ngClass]="field.ngClass">
+            <ng-container *ngFor="let subfield of field.collections;" klesDynamicField [field]="subfield" [group]="subGroup">
             </ng-container>
         </div>
     </div>
 `,
     styles: ['mat-form-field {width: calc(100%)}',
-        ':host { display:inherit; flex-direction: inherit}'
+        ':host { display:inherit; flex-direction: inherit}',
+        '.group-container {display:inherit; flex-direction: inherit}'
     ]
 })
 export class KlesFormGroupComponent extends KlesFieldAbstract implements OnInit {
