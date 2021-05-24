187883f6058f93b989f7a47a13c2a055886d2401
jchaut <j.chaut@3kles-consulting.com>
2021-05-24 12:46:09 +0200

    Add optional property on autocomplete validator

--- projects/kles-material-dynamicforms/src/lib/validators/autocomplete.validator.ts
+++ projects/kles-material-dynamicforms/src/lib/validators/autocomplete.validator.ts
@@ -1,16 +1,18 @@
 import { AbstractControl, ValidatorFn } from "@angular/forms"
 
-export function autocompleteObjectValidator(): ValidatorFn {
+export function autocompleteObjectValidator(optional?: boolean): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
         if (typeof control.value === 'string') {
+            if (control.value === '' && optional) return null;
             return { 'invalidAutocompleteObject': { value: control.value } }
         }
         return null  /* valid option selected */
     }
 }
 
-export function autocompleteStringValidator(validOptions: Array<string>): ValidatorFn {
+export function autocompleteStringValidator(validOptions: Array<string>, optional?: boolean): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
+        if (control.value === '' && optional) return null;
         if (validOptions.indexOf(control.value) !== -1) {
             return null  /* valid option selected */
         }
