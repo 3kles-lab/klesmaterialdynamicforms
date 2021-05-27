7854d84db1b9d86871b6e2613477bcaf3c5e1f13
jchaut <j.chaut@3kles-consulting.com>
2021-05-27 11:34:05 +0200

    Fix autocomplete validator

--- projects/kles-material-dynamicforms/src/lib/validators/autocomplete.validator.ts
+++ projects/kles-material-dynamicforms/src/lib/validators/autocomplete.validator.ts
@@ -2,6 +2,8 @@ import { AbstractControl, ValidatorFn } from "@angular/forms"
 
 export function autocompleteObjectValidator(optional?: boolean): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
+        if (control.value === null && !optional) return { 'invalidAutocompleteObject': { value: control.value } }
+
         if (typeof control.value === 'string') {
             if (control.value === '' && optional) return null;
             return { 'invalidAutocompleteObject': { value: control.value } }
@@ -12,7 +14,7 @@ export function autocompleteObjectValidator(optional?: boolean): ValidatorFn {
 
 export function autocompleteStringValidator(validOptions: Array<string>, optional?: boolean): ValidatorFn {
     return (control: AbstractControl): { [key: string]: any } | null => {
-        if (control.value === '' && optional) return null;
+        if ((control.value === '' || control.value === null) && optional) return null;
         if (validOptions.indexOf(control.value) !== -1) {
             return null  /* valid option selected */
         }
