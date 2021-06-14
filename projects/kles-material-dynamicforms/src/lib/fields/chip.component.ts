20bebae7f059363d46238e8529e4b818786f5618
Charles Da Silva Costa <cdasilvacosta@3kles-consulting.com>
2021-06-14 15:44:12 +0200

    Add klestransform pipe in chip component

--- projects/kles-material-dynamicforms/src/lib/fields/chip.component.ts
+++ projects/kles-material-dynamicforms/src/lib/fields/chip.component.ts
@@ -8,7 +8,7 @@ import { KlesFieldAbstract } from "./field.abstract";
         <mat-chip-list>
             <mat-chip [color]="field.color" matTooltip="{{field.tooltip}}" [attr.id]="field.id" selected [ngClass]="field.ngClass" [ngStyle]="field.ngStyle">
                 <mat-icon *ngIf="field.icon">{{field.icon}}</mat-icon>
-                {{group.controls[field.name].value}}
+                {{group.controls[field.name].value | klesTransform:field.pipeTransform}}
             </mat-chip>
         </mat-chip-list>
     </div>
