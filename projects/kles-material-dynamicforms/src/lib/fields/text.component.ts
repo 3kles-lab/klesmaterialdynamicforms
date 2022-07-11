29e6f0859da346a2652dbd574be75c2867cd6ecc
Charles Da Silva Costa <cdasilvacosta@3kles-consulting.com>
2022-07-11 17:25:52 +0200

    Fix text component

--- projects/kles-material-dynamicforms/src/lib/fields/text.component.ts
+++ projects/kles-material-dynamicforms/src/lib/fields/text.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnDestroy, OnInit } from "@angular/core";
-import { KlesFieldAbstract } from "./field.abstract";
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { KlesFieldAbstract } from './field.abstract';
 
 @Component({
     selector: 'kles-form-text',
     template: `
     <span matTooltip="{{field.tooltip}}" [attr.id]="field.id" [ngClass]="field.ngClass">
-        {{(field.property ? group.controls[field.name].value[field.property] : group.controls[field.name].value) | klesTransform:field.pipeTransform}}
-    </span> 
+        {{((field.property && group.controls[field.name].value) ? group.controls[field.name].value[field.property] : group.controls[field.name].value) | klesTransform:field.pipeTransform}}
+    </span>
 `
 })
 export class KlesFormTextComponent extends KlesFieldAbstract implements OnInit, OnDestroy {
