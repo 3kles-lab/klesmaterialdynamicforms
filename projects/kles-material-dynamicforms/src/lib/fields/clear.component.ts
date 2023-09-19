ab12a7f52dbd3c0fe40ff51cfbc11bb396a462a9
Charles Da Silva Costa <CDASILVA@3KLES.local>
2023-09-19 12:03:39 +0200

    Change clear component visibility

--- projects/kles-material-dynamicforms/src/lib/fields/clear.component.ts
+++ projects/kles-material-dynamicforms/src/lib/fields/clear.component.ts
@@ -6,7 +6,7 @@ import { IKlesFieldConfig } from "../interfaces/field.config.interface";
 @Component({
     selector: 'kles-form-clear',
     template: `
-    <button *ngIf="group.get(field.name).value && !group.get(field.name).disabled" mat-icon-button aria-label="Clear" type="button"
+    <button [disabled]="!group.get(field.name).value || group.get(field.name).disabled" mat-icon-button aria-label="Clear" type="button"
         (click)="clear($event)">
         <mat-icon>close</mat-icon>
     </button>
