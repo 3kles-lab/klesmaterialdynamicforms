e314f82c88faf24f0e0dd6084d2d9969904e36f8
Charles Da Silva Costa <CDASILVA@3KLES.local>
2026-09-01 15:04:13 +0200

    fix: update mat error form to use signal

--- projects/kles-material-dynamicforms/src/lib/directive/mat-error-form.directive.ts
+++ projects/kles-material-dynamicforms/src/lib/directive/mat-error-form.directive.ts
@@ -1,12 +1,15 @@
 import { Component, computed, input } from '@angular/core';
 import { AsyncValidatorFn, FormsModule, ReactiveFormsModule, UntypedFormGroup, ValidatorFn } from '@angular/forms';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { map, startWith, switchMap } from 'rxjs';
+
 import { IKlesValidator } from '../interfaces/validator.interface';
 import { flattenValidators } from '../utils/validation.util';
 
 @Component({
     selector: '[matErrorForm]',
     template: `
-        @if (form().errors) {
+        @if (formErrors()) {
             @for (validation of validationsKeys(); track validation.name) {
                 @if (form().hasError(validation.name) && validation.message) {
                     {{ validation.message }}
@@ -25,8 +28,24 @@ import { flattenValidators } from '../utils/validation.util';
 })
 export class MatErrorFormDirective {
     readonly form = input.required<UntypedFormGroup>();
-    readonly validations = input<IKlesValidator<ValidatorFn>[]>();
-    readonly asyncValidations = input<IKlesValidator<AsyncValidatorFn>[]>();
-    readonly validationsKeys = computed(() => flattenValidators<ValidatorFn>(this.validations() ?? []));
-    readonly asyncValidationsKeys = computed(() => flattenValidators<AsyncValidatorFn>(this.asyncValidations() ?? []));
+
+    readonly validations = input<IKlesValidator<ValidatorFn>[]>([]);
+
+    readonly asyncValidations = input<IKlesValidator<AsyncValidatorFn>[]>([]);
+
+    readonly validationsKeys = computed(() => flattenValidators<ValidatorFn>(this.validations()));
+
+    readonly asyncValidationsKeys = computed(() => flattenValidators<AsyncValidatorFn>(this.asyncValidations()));
+
+    readonly formErrors = toSignal(
+        toObservable(this.form).pipe(
+            switchMap((form) =>
+                form.statusChanges.pipe(
+                    startWith(form.status),
+                    map(() => form.errors),
+                ),
+            ),
+        ),
+        { initialValue: null },
+    );
 }
